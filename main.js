b43ba67c081840222fea147ad9a6eba2f863678c
nGeorgit <113199881+nGeorgit@users.noreply.github.com>
2023-03-03 16:40:11 +0200

    Add files via upload

--- main.js
+++ main.js
@@ -1,3 +1,10 @@
+pathD = new Array()
+curNode = null
+graph = {}
+node = null
+curFloorId = null
+mergeData = null
+navArray = null
 
 fileInp.onchange = evt => {
     const [file] = fileInp.files
@@ -7,24 +14,386 @@ fileInp.onchange = evt => {
         reader.onload = function(e) {
             JSONdata = e.target.result
             JSONdata = JSON.parse(JSONdata)
-            pathToPol(findPath(JSONdata.grid, 3, 3, 3, 4))
+            floorsN = JSONdata.floors.length
+            graph = JSONdata.graph
+            mergeData = JSONdata
 
         }
        
     }
     
-    }
+}
 
 const map = L.map('map', {
     crs: L.CRS.Simple,
     minZoom: -5
 });
 
-var bounds = [[0,0], [1656,2319]];
-var image = L.imageOverlay('4gd.jpg', bounds).addTo(map);
-map.fitBounds(bounds);
+///
 
+class pathNode {
+  constructor(name, floorId, cords)
+  {
+    this.name = name
+    this.floorId = floorId
+    this.cords = cords
+  }
+}
+
+class pathEntryNode extends pathNode {
+  constructor(code, floorId, cords, level, id)
+  {
+    super(code, floorId, cords)
+    this.level = level
+    this.id = id
+  }
+}
+
+class navPoint {
+  constructor(floorId, rows, res, nodeSize, text, pathNode)
+  {
+    this.floorId = floorId
+    this.rows = rows
+    this.res = res
+    this.nodeSize = nodeSize
+    this.text1 = text
+    this.pathNode1 = pathNode
+  }
+
+  setMap()
+  {
+    clearMap()
+    var bounds = [[0,0], [this.res.height, this.res.width]];
+    var image = L.imageOverlay('demoMaps/'+ this.floorId +'.png', bounds).addTo(map);
+    map.fitBounds(bounds);
+  }
+
+  show() 
+  {
+    this.setMap()
+    L.marker(L.latLng((this.rows - this.pathNode1.cords.y) * this.nodeSize, this.pathNode1.cords.x * this.nodeSize)).addTo(map).bindPopup(this.text1);
+  }
+
+}
+
+class navPath extends navPoint {
+  constructor(floorId, rows, res, nodeSize, text1, pathNode1, text2, pathNode2, grid)
+  {
+    super(floorId, rows, res, nodeSize, text1, pathNode1)
+    this.text2 = text2
+    this.pathNode2 = pathNode2
+    this.grid = grid
+  }
+
+  findPath(){
+    var graphA = new Graph(invers(this.grid), { diagonal: true });
+    var start = graphA.grid[this.pathNode1.cords.x][this.pathNode1.cords.y]
+    var end = graphA.grid[this.pathNode2.cords.x][this.pathNode2.cords.y]
+    var result = astar.search(graphA, start, end)
+    result.unshift(start);
+    
+    return result
+  }
+
+  pathToPol(result){
+    var pol = new Array()
+    
+    console.log(result)
+    for (i = 0; i < result.length; i++) {
+      pol[i] = L.latLng((this.rows - result[i].y) * this.nodeSize, result[i].x * this.nodeSize )
+    }
   
+    //L.marker(L.latLng((formY(result[0].y)) * floor.nodeSize, result[0].x * floor.nodeSize)).addTo(map).bindPopup('a');
+    L.polyline(pol).addTo(map)
+  }
+
+  show()
+  {
+    this.setMap()
+    this.pathToPol(this.findPath())
+  }
+
+}
+
+function getPathWithNodes(pathD)
+{
+  nodePath = new Array()
+  start = mergeData["places"][pathD[0]]
+  nodePath[0] = new pathNode(pathD[0], start.floorId, start.cords)
+  
+  for(i = 1; i < pathD.length -1; i++)
+  {
+    entry = mergeData["entries"][pathD[i]]
+    nodePath[i] = new pathEntryNode(pathD[i], entry.floorId, entry.cords, entry.level, entry.id)
+  }
+
+  end = mergeData["places"][pathD[pathD.length - 1]]
+  nodePath[pathD.length - 1] = new pathNode(pathD[pathD.length - 1], end.floorId, end.cords)
+
+  return nodePath
+}
+
+function makeNav(nodePath)
+{
+  navArray = new Array()
+
+  k = 0
+  //curFloor = mergeData["floors"][nodePath[0].floorId]
+  //navArray[k] = new navPath(nodePath[0].floorId, curFloor.rows, curFloor.res, curFloor.nodeSize, "Start", nodePath[0], "", nodePath[1], curFloor.grid)
+
+  i = 0
+  while (i < nodePath.length - 1)
+  {
+    if(k%2==0)
+    {
+      curFloor = mergeData["floors"][nodePath[i].floorId]
+      navArray[k] = new navPath(curFloor.id, curFloor.rows, curFloor.res, curFloor.nodeSize, "Start", nodePath[i], "", nodePath[i + 1], curFloor.grid)
+      i++
+      k++
+    } else
+    {
+      while(nodePath[i].floorId != nodePath[i + 1].floorId)
+      {
+        i++
+      }
+      curFloor = mergeData["floors"][nodePath[i].floorId]
+      navArray[k] = new navPoint(curFloor.id, curFloor.rows, curFloor.res, curFloor.nodeSize, "", nodePath[i])
+      k++
+    }
+  }
+
+  return navArray
+}
+
+///
+
+function find()
+{
+  start = document.getElementById('start').value//"0_0_0"
+  end = document.getElementById('end').value//"1_1_1"
+
+  addPlacesToGraph(start, end)
+
+  graphD = new GraphD(graph);
+  pathD = graphD.findShortestPath(start, end)
+  navArray = makeNav(getPathWithNodes(pathD))
+  console.log(graph)
+  curNode = 0
+  navArray[curNode].show()
+};
+
+function next()
+{
+  if (curNode != navArray.length - 1)
+  {
+    curNode++
+    navArray[curNode].show()
+  }
+}
+
+function prev()
+{
+  if (curNode != 0)
+  {
+    curNode--
+    navArray[curNode].show()
+  }
+}
+
+function addPlacesToGraph(start, end)
+{
+  updateGraph(start)
+  updateGraph(end)
+}
+
+function updateGraph(name)
+{
+  place = mergeData["places"][name] //getPlaceFromName(name, floor)
+  costs = {}
+  
+  entries = Object.keys(mergeData["floors"][place.floorId]["entries"])
+  for(entName in entries)
+  {
+    entry = mergeData["entries"][entries[entName]]
+    console.log(entName)
+    cost = findPath(mergeData["floors"][place.floorId].grid, place.cords.x, place.cords.y, entry.cords.x, entry.cords.y).length
+
+    if (graph[name] == undefined)
+    {
+      graph[name] = {}
+    }
+    
+    graph[name][entries[entName]] = cost
+    graph[entries[entName]][name] = cost 
+    console.log(graph)
+
+  }
+
+}
+
+function nav2D(floorId, startX, startY, endX, endY)
+{
+  clearMap()
+  curFloor = getFloorData(floorId)
+  var bounds = [[0,0], [curFloor.res.height,curFloor.res.width]];
+  var image = L.imageOverlay('demoMaps/'+ floorId +'.png', bounds).addTo(map);
+  map.fitBounds(bounds);
+  pathToPol(findPath(curFloor.grid, startX, startY, endX, endY), floor)
+}
+
+function nav()
+{
+  console.log(curNode)
+
+  if (curNode != 0 && (curNode != pathD.length - 1))
+  {
+    curFloorId = getFloorFromCode(pathD[curNode])
+    floor = getFloorData(curFloorId)
+    node = getEntry(pathD[curNode], floor)
+  } else
+  {
+    floor = getFloorData(getFloorFromPlace(pathD[curNode]))
+    node = getPlaceFromName(pathD[curNode], floor)
+    curFloorId = floor.id
+  }
+  
+
+  next = pathD[curNode + 1] || -1
+  if (next != -1)
+  {
+
+    if (curFloorId == getFloorFromCode(next) || curNode == pathD.length-2)
+    {
+      nextNode = getNextNode(next)
+      nav2D(curFloorId, node.cords.x, node.cords.y, nextNode.cords.x, nextNode.cords.y)
+    } else
+    {
+      showPoint(floor, node, curNode)
+    }
+  } else
+  {
+    showPoint(floor, node, "Finish")
+  }
+}
+
+function front()
+{
+  curNode++
+  updateCurNode()
+  if (pathD[curNode]!= undefined)
+  {
+    next = pathD[curNode + 1]
+    nextNode = getNextNode(next)
+    while(curNode == pathD.length-2)
+    {
+      
+      if (curFloorId != getFloorFromCode(next))
+      {
+        curNode++
+        updateCurNode()
+        next = pathD[curNode + 1] || -1
+      }else{ break}
+    }
+    
+    nav()
+  }
+}
+
+function updateCurNode()
+{
+  if (curNode != 0 && (curNode != pathD.length - 1))
+  {
+    curFloorId = getFloorFromCode(pathD[curNode])
+    floor = getFloorData(curFloorId)
+    node = getEntry(pathD[curNode], floor)
+  } else
+  {
+    floor = getFloorData(getFloorFromPlace(pathD[curNode]))
+    node = getPlaceFromName(pathD[curNode], floor)
+    curFloorId = floor.id
+  }
+}
+
+function getNextNode(next)
+{
+  if (curNode == pathD.length-2)
+  {
+    floor = getFloorData(curFloorId)
+    return getPlaceFromName(pathD[curNode], floor)
+  } else
+  {
+    floor =getFloorData(getFloorFromCode(next))
+    return getEntry(next, floor)
+  }
+}
+
+function clearMap()
+{
+  map.eachLayer(function (layer) {
+    map.removeLayer(layer);
+  });
+}
+
+function showPoint(floor, curEnt, text)
+{
+  clearMap()
+  var bounds = [[0,0], [floor.res.height,floor.res.width]];
+  var image = L.imageOverlay('demoMaps/'+ curFloorId +'.png', bounds).addTo(map);
+  map.fitBounds(bounds);
+  L.marker(L.latLng((floor.rows - curEnt.cords.y) * floor.nodeSize, curEnt.cords.x * floor.nodeSize)).addTo(map).bindPopup(text);
+}
+
+function getFloorData(floorId)
+{
+  for (i = 0; i < floorsN; i++)
+  {
+    if (JSONdata.floors[i].id == floorId)
+    {
+      return JSONdata.floors[i]
+    }
+  }
+}
+
+function getFloorFromCode(code)
+{
+  return code.split('_')[2]
+}
+
+function getEntry(code, floor)
+{
+  for (i = 0; i < floor.entries.length; i++)
+  {
+    if ( floor.entries[i].code == code)
+    {
+      return floor.entries[i]
+    }
+  }
+}
+
+function getFloorFromPlace(name)
+{
+  for (i = 0; i < JSONdata.places.length; i++)
+  {
+    if (name == JSONdata.places[i].name)
+    {
+      return JSONdata.places[i].floorId
+    }
+  }
+}
+
+function getPlaceFromName(name, floor)
+{
+  //console.log(floor)
+  for (i = 0; i < floor.places.length; i++)
+  {
+    if (name == floor.places[i].name)
+    {
+      return floor.places[i]
+    }
+  }
+}
+
+//////////
 function pathTo(node) {
   var curr = node;
   var path = [];
@@ -404,30 +773,32 @@ BinaryHeap.prototype = {
   }
 };
 
+//////////
+
 function formY(y){
   return JSONdata.rows - y
 }
 
 function findPath(matrix, startX, startY, endX, endY){
-  var graph = new Graph(invers(matrix), { diagonal: true });
-
-  var start = graph.grid[startX][startY]
-  var end = graph.grid[endX][endY]
-  var result = astar.search(graph, start, end)
+  var graphA = new Graph(invers(matrix), { diagonal: true });
+  var start = graphA.grid[startX][startY]
+  var end = graphA.grid[endX][endY]
+  var result = astar.search(graphA, start, end)
   result.unshift(start);
-  console.log(result)
+  
 
   return result
 }
 
 
-function pathToPol(result){
+function pathToPol(result, floor){
   var pol = new Array()
+  
   for (i = 0; i < result.length; i++) {
-    pol[i] = L.latLng(formY(result[i].y) * JSONdata.nodeSize,result[i].x * JSONdata.nodeSize )
+    pol[i] = L.latLng((floor.rows - result[i].y) * floor.nodeSize, result[i].x * floor.nodeSize )
   }
 
-  L.marker(L.latLng((formY(result[0].y)) * JSONdata.nodeSize, result[0].x * JSONdata.nodeSize)).addTo(map).bindPopup('a');
+  //L.marker(L.latLng((formY(result[0].y)) * floor.nodeSize, result[0].x * floor.nodeSize)).addTo(map).bindPopup('a');
   L.polyline(pol).addTo(map)
 }
 
@@ -438,7 +809,6 @@ function invers(matrix) {
   for (i = 0; i < matrix[0].length; i++){
     invMatr[i] = new Array()
   }
-  console.log(invMatr)
   for (i = 0; i < matrix.length; i++){
     for (j = 0; j < matrix[0].length;  j++){
       invMatr[j][i] = matrix[i][j]
@@ -446,3 +816,155 @@ function invers(matrix) {
   }
   return invMatr
 }
+
+////// dij
+
+
+var GraphD = (function (undefined) {
+
+	var extractKeys = function (obj) {
+		var keys = [], key;
+		for (key in obj) {
+		    Object.prototype.hasOwnProperty.call(obj,key) && keys.push(key);
+		}
+		return keys;
+	}
+
+	var sorter = function (a, b) {
+		return parseFloat (a) - parseFloat (b);
+	}
+
+	var findPaths = function (map, start, end, infinity) {
+		infinity = infinity || Infinity;
+
+		var costs = {},
+		    open = {'0': [start]},
+		    predecessors = {},
+		    keys;
+
+		var addToOpen = function (cost, vertex) {
+			var key = "" + cost;
+			if (!open[key]) open[key] = [];
+			open[key].push(vertex);
+		}
+
+		costs[start] = 0;
+
+		while (open) {
+			if(!(keys = extractKeys(open)).length) break;
+
+			keys.sort(sorter);
+
+			var key = keys[0],
+			    bucket = open[key],
+			    node = bucket.shift(),
+			    currentCost = parseFloat(key),
+			    adjacentNodes = map[node] || {};/////////////////////////
+
+			if (!bucket.length) delete open[key];
+
+			for (var vertex in adjacentNodes) {
+			    if (Object.prototype.hasOwnProperty.call(adjacentNodes, vertex)) {
+					var cost = adjacentNodes[vertex],///////////////////////////////////////
+					    totalCost = cost + currentCost,
+					    vertexCost = costs[vertex];
+
+					if ((vertexCost === undefined) || (vertexCost > totalCost)) {
+						costs[vertex] = totalCost;
+						addToOpen(totalCost, vertex);
+						predecessors[vertex] = node;
+					}
+				}
+			}
+		}
+
+		if (costs[end] === undefined) {
+			return null;
+		} else {
+			return predecessors;
+		}
+
+	}
+
+	var extractShortest = function (predecessors, end) {
+		var nodes = [],
+		    u = end;
+
+		while (u !== undefined) {
+			nodes.push(u);
+			u = predecessors[u];
+		}
+
+		nodes.reverse();
+		return nodes;
+	}
+
+	var findShortestPath = function (map, nodes) {
+		var start = nodes.shift(),
+		    end,
+		    predecessors,
+		    path = [],
+		    shortest;
+
+		while (nodes.length) {
+			end = nodes.shift();
+			predecessors = findPaths(map, start, end);
+
+			if (predecessors) {
+				shortest = extractShortest(predecessors, end);
+				if (nodes.length) {
+					path.push.apply(path, shortest.slice(0, -1));
+				} else {
+					return path.concat(shortest);
+				}
+			} else {
+				return null;
+			}
+
+			start = end;
+		}
+	}
+
+	var toArray = function (list, offset) {
+		try {
+			return Array.prototype.slice.call(list, offset);
+		} catch (e) {
+			var a = [];
+			for (var i = offset || 0, l = list.length; i < l; ++i) {
+				a.push(list[i]);
+			}
+			return a;
+		}
+	}
+
+	var GraphD = function (map) {
+		this.map = map;
+	}
+
+	GraphD.prototype.findShortestPath = function (start, end) {
+		if (Object.prototype.toString.call(start) === '[object Array]') {
+			return findShortestPath(this.map, start);
+		} else if (arguments.length === 2) {
+			return findShortestPath(this.map, [start, end]);
+		} else {
+			return findShortestPath(this.map, toArray(arguments));
+		}
+	}
+
+	GraphD.findShortestPath = function (map, start, end) {
+		if (Object.prototype.toString.call(start) === '[object Array]') {
+			return findShortestPath(map, start);
+		} else if (arguments.length === 3) {
+			return findShortestPath(map, [start, end]);
+		} else {
+			return findShortestPath(map, toArray(arguments, 1));
+		}
+	}
+
+	return GraphD;
+
+})();
+
+
+
+//////
